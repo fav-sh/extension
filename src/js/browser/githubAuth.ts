ac635065e964b217b66805ae2cba4a76c8306376
Sunny Golovine <sunny@glvn.co>
2020-03-14 17:34:14 -0400

    Chrome intergration almost complete

--- src/js/browser/githubAuth.ts
+++ src/js/browser/githubAuth.ts
@@ -1,6 +1,15 @@
+const clientId = '5e8dad696b12cb5f6868'
+const scope = 'gist'
+
 export async function authorize() {
-  const clientId = '5e8dad696b12cb5f6868'
-  const scope = 'gist'
+  if (process.env.TARGET === 'chrome') {
+    return chromeVersion()
+  } else {
+    return browserVersion()
+  }
+}
+
+async function browserVersion() {
   const redirectURL = browser.identity.getRedirectURL()
   const authURL = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectURL}`
 
@@ -8,6 +17,21 @@ export async function authorize() {
     interactive: true,
     url: authURL,
   })
-
   return auth.split('?code=')[1]
+}
+
+function chromeVersion() {
+  const redirectURL = chrome.identity.getRedirectURL()
+  const authURL = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectURL}`
+
+  return chrome.identity.launchWebAuthFlow(
+    {
+      interactive: true,
+      url: authURL,
+    },
+    (responseUrl?: string) => {
+      console.log(responseUrl)
+      return responseUrl && responseUrl.split('?code=')[1]
+    }
+  )
 }
