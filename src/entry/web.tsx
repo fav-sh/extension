21ee4249aced87c6bd6b0d8c21c3763aa61f8d0e
Sunny Golovine <sunny@glvn.co>
2020-04-12 23:46:47 -0400

    Add report script, remove web packages

--- src/entry/web.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * This is the entry point for the
- * extension
- */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { WebBootstrap } from '../js/App'
-
-ReactDOM.render(<WebBootstrap />, document.getElementById('root'))
